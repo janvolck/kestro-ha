2fdb11db5285dcfd53a2923e2fca777b127c3c8b
Jan Volckaert <janvolck@gmail.com>
2025-02-23 20:20:27 +0100

    update module

---
+++ python/kestro_iot/webapp/controllers/peripherals_controller.py
@@ -0,0 +1,83 @@
+from flask import Blueprint, jsonify, abort, request
+
+from ...peripherals.peripheral_service import PeripheralService
+
+peripheral_service: PeripheralService = None
+
+api = Blueprint("kestro_peripherals", __name__, url_prefix="/api/kestro/peripherals")
+
+
+@api.route("/gpios")
+def gpios_status():
+    if not peripheral_service:
+        abort(500)
+
+    return jsonify(peripheral_service.gpio().status())
+
+
+@api.route("/gpios/<string:pin_id>")
+def gpio_pin_status(pin_id):
+    if not peripheral_service:
+        abort(500)
+
+    return jsonify(peripheral_service.gpio().pin_status(pin_id))
+
+
+@api.route("/gpios/<string:pin_id>/on", methods=["POST"])
+def on(pin_id):
+    if not peripheral_service:
+        abort(500)
+
+    peripheral_service.gpio().enable(pin_id)
+    return ("", 204)
+
+
+@api.route("/gpios/<string:pin_id>/off", methods=["POST"])
+def off(pin_id):
+    if not peripheral_service:
+        abort(500)
+
+    peripheral_service.gpio().disable(pin_id)
+    return ("", 204)
+
+
+@api.route("/gpios/<string:pin_id>/toggle", methods=["POST"])
+def toggle(pin_id):
+    if not peripheral_service:
+        abort(500)
+
+    peripheral_service.gpio().toggle(pin_id)
+    return ("", 204)
+
+
+@api.route("/sensors/power")
+def power_status():
+    if not peripheral_service:
+        abort(500)
+
+    if not peripheral_service.power_sensor():
+        abort(404)
+
+    return jsonify(peripheral_service.power_sensor().status())
+
+
+@api.route("/sensors/temperature")
+def temperature_status():
+    if not peripheral_service:
+        abort(500)
+
+    if not peripheral_service.temperature_sensor():
+        abort(404)
+
+    return jsonify(peripheral_service.temperature_sensor().status())
+
+
+@api.route("/sensors/humidity")
+def humidity_status():
+    if not peripheral_service:
+        abort(500)
+
+    if not peripheral_service.humidity_sensor():
+        abort(404)
+
+    return jsonify(peripheral_service.humidity_sensor().status())
